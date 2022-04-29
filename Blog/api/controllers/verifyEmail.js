914aef21bd324ffe1154f92c4d45c5b34faa7258
bayustrio7 <bayustrio7@gmail.com>
2022-04-30 01:05:42 +0700

    feat: update template email  🦊

--- Blog/api/controllers/verifyEmail.js
+++ Blog/api/controllers/verifyEmail.js
@@ -1,18 +1,18 @@
-
+const images = require("../../images/email.png");
 module.exports = (transporter, user, req, res) => {
   let mailOptions = {
     from: ` "Verify your email" <${process.env.EMAIL_USERNAME}>`,
     to: user.email,
     subject: "roxyzc -Verify your email",
     html: `
-            <div style="max-width: 700px; display:flex; justify-content:center; align-items:center; background-color:#F7F9FA;
-                <img src='../../images/email.png/>
+            <div style="max-width: 700px; display:flex; justify-content:center; align-items:center; background-color:#F7F9FA";
+                <img src=${images}/>
             <div>
-        <h3> ${user.nama}! Thanks for registering in our site </h3>
+
+        <h3 ><span style="font-size: bold "> ${user.nama} </span>! Thanks for registering in our site </h3>
                 <h4> Just click the button below to validate your email address. </h4>
                 <a target="_blank" href="http://${req.headers.host}/api/verify-email?token=${user.tokenEmail}">Verif email</a>
                 </div>
-
                 </div>
                 `,
   };
